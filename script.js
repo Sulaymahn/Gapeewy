f20330b75d7ae895723a97fec1034fe45ff21014
Sulaymahn Mukhtar <sulmuk28@gmail.com>
2024-07-15 21:55:35 +0100

    new changes

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-class CustomFooter extends HTMLElement {
-    constructor() {
-        super();
-    }
+const offset = -200;
+let isSticky = false;
 
-    connectedCallback() {
-        //const shadow = this.attachShadow({ mode: "open" })
-        this.innerHTML = 
-        `
-        <footer>
-        <div class="container">
-            <div>
-                <img src="/assets/images/gapeewy_logo_light.png" style="height: 48px; margin-bottom: 32px;">
-                <div>
-                    <a href="https://www.instagram.com/gapeewy/"><i class="bi bi-instagram text-white"></i></a>
-                    <a href="https://x.com/gapeewy"><i class="bi bi-twitter text-white"></i></a>
-                    <a href="https://web.facebook.com/gapeewy"><i class="bi bi-facebook text-white"></i></a>
-                    <a href="https://www.tiktok.com/@gapeewy"><i class="bi bi-tiktok text-white"></i></a>
-                </div>
-            </div>
-            <div class="footer-link-container">
-                <div>
-                    <h6 style="margin-bottom: 16px;">Gapeewy</h6>
-                    <a style="margin: 0; text-decoration: none; color: white;" href="aboutus.html">About us</a>
-                    <p style="margin: 0;">FAQ</p>
-                </div>
-                <div class="text-end">
-                    <h6 style="margin-bottom: 16px;">Contact us</h6>
-                    <a class="social-link" href="mailto:gapeewy@gmail.com">gapeewy@gmail.com</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-        `
-    }
+const navBar = document.getElementById('navigation-bar');
+const hero_one = document.getElementById('hero-one');
+const navigation_brand_image = document.getElementById('navigation-brand-logo');
+let slideId = null;
+
+if (navBar) {
+    window.addEventListener('scroll', (event) => {
+        if (!isSticky && hero_one.getBoundingClientRect().top < offset) {
+            navBar.style.transform = "translateY(" + (-navBar.getBoundingClientRect().height) + "px)";
+            navigation_brand_image.src = "assets/images/gapeewy_logo.png";
+            isSticky = true;
+            clearTimeout(slideId);
+
+            slideId = setTimeout(() => {
+                navBar.classList.add("nav-sticky");
+                navBar.style.transform = "translateY(0)";
+            }, 200);
+        } else if (isSticky && hero_one.getBoundingClientRect().top >= offset) {
+            navBar.classList.remove("nav-sticky");
+            navigation_brand_image.src = "assets/images/gapeewy_logo_light.png";
+            navBar.style.transform = "";
+            isSticky = false;
+        }
+    });
 }
-
-customElements.define("app-footer", CustomFooter);
